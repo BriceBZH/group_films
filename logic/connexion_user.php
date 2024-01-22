6193b369464465c5ebc0eb5c8b76c10f19d3f5ee
jlow56 <jeanlougames@hotmail.com>
2024-01-22 15:03:21 +0000

    Ajout de header.phtml,logic/connexion_user.php modification dans connexion.php,création de logic/delet_session

--- logic/connexion_user.php
+++ logic/connexion_user.php
@@ -2,23 +2,33 @@
 // Démarre la session
 require "../config/connexion.php";
 // Vérifie si l'utilisateur est déjà connecté
-if (isset($_SESSION['user'])) {
-    header("Location: connected.php");
+if (isset($_SESSION['user'])) 
+
+{
+    header("Location: ../index.php");
     exit;
 }
 
 // Vérifie si le formulaire a été soumis
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Vous pouvez ajouter la logique d'authentification ici
-    // par exemple, vérifier le nom d'utilisateur et le mot de passe dans la base de données
-    // Si l'authentification réussit, enregistrez l'utilisateur dans la session
-    $_SESSION['user'] = $_POST['username'] && $_SESSION['user'] = $_POST ['password'];
-    header("Location: connected.php");
-    exit;
-    $isPasswordCorrect = password_verify($_POST['password'], $user['password']);
-if ($isPasswordCorrect) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 
+{
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT);
     
+    $query = "SELECT * FROM  Users WHERE pseudo = :pseudo"; 
+    $statement->bindParam(':pseudo', $pseudo);
+    $statement->execute();
+    $pseudo = $statement->fetch();
+    $_SESSION['idUser'] = $user['id'];
+    
+     if (!empty($pseudo) && password_verify($password, $pseudo['password'])) 
+     {
+        // si l'authentification réussie, enregistre l'utilisateur dans la session
+        $_SESSION['pseudo'] = $pesudo['pseudo'];
+        $_SESSION['idUser'] = $pseudo['id'];
+     }
 }
 header('Location: ../index.php');
 ?>
