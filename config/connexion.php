fed00aa10f3f3c3deb09446e9ebf481ac91fe188
adan-gilbert <adan.gilbert@3wa.io>
2024-01-22 13:52:05 +0000

    affichage terminer, début de la phase modifié

--- config/connexion.php
+++ config/connexion.php
@@ -1,11 +1,11 @@
 <?php
 $host = "db.3wa.io";
 $port = "3306";
-$dbname = "bricerubeaux_phpj5";
-$connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
+$dbname = "adangilbert_groups_films";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
-$user = "bricerubeaux";
-$password = "dfcef705866daf59347bbd9795fbe016";
+$user = "adangilbert";
+$password = "dfdea6b0f73368f1dd0954ba9bbcca30";
 
 $db = new PDO(
     $connexionString,
